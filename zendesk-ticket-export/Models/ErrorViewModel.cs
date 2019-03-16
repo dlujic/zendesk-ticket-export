9010fb75a142b60a59e6578ccc8a6267857f9a0c
Dejan Lujic <darkwanderer90@gmail.com>
2019-03-16 12:17:04 +0100

    Add project files.

---
+++ zendesk-ticket-export/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace zendesk_ticket_export.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
